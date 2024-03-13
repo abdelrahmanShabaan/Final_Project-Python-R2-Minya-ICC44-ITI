27e221b22d74116d7041ad89bf29bcb16d875e7f
hassaneldash <hassan.eldash@yahoo.com>
2024-03-13 17:13:01 +0200

    NewAfterMath

--- frontend/src/store/reducers/AddToFav.js
+++ frontend/src/store/reducers/AddToFav.js
@@ -1,23 +1,10 @@
-const initialState = {
-  favorites: [],
-  favoritesCount: 0,
-};
-
-const favoritesReducer = (state = initialState, action) => {
+const favoritesReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_TO_FAVORITES":
-      return {
-        ...state,
-        favorites: [...state.favorites, action.payload],
-        favoritesCount: state.favoritesCount + 1,
-      };
+      return [...state, action.payload];
 
     case "REMOVE_FROM_FAVORITES":
-      return {
-        ...state,
-        favorites: state.favorites.filter((product) => product.id !== action.payload),
-        favoritesCount: state.favoritesCount - 1,
-      };
+      return state.filter((product) => product.id !== action.payload);
 
     default:
       return state;
